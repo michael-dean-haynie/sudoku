72b08c1d6d9aea786a89f26caf963990acd94a96
Michael Haynie <michael.dean.haynie@gmail.com>
2022-08-18 14:07:35 -0700

    add test blocks in no-duplicates_test.cc

--- test/strategies/no-duplicates_test.cc
+++ test/strategies/no-duplicates_test.cc
@@ -74,3 +74,35 @@ TEST(NoDuplictesTests, ShouldReturnProgressEventIfColHasValue) {
 
 }
 
+TEST(NoDuplictesTests, ShouldReturnProgressEventIfBlockHasValue) {
+    // arrange
+    Grid *grid = createEmptyGrid();
+    (*grid)[0][0] = createCell(0, 0, 0); // cell to solve
+    (*grid)[2][2] = createCell(2, 2, 5); // cell with value
+
+    ProgressEvent expected;
+    expected.strat_p = strdup("noDuplicatesStrat");
+    expected.row = 0;
+    expected.col = 0;
+    expected.oldValue = 0;
+    expected.newValue = 0;
+    for(int i = 0; i <= MAX_VAL; i++) {
+        if (i == 0) { // notes at [0] is always 0
+            expected.oldNotes[i] = 0;
+            expected.newNotes[i] = 0;
+        } else {
+            expected.oldNotes[i] = 1;
+            expected.newNotes[i] = 1;
+        }
+    }
+    expected.newNotes[5] = 0;
+
+    // act
+    ProgressEvent *actual = noDuplicatesStrat(grid, 0, 0);
+    printProgressEvent(&expected);
+    printProgressEvent(actual);
+
+    // assert
+    ASSERT_TRUE(progressEventsAreEqual(&expected, actual));
+
+}
